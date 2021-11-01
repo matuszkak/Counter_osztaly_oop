059e93a51acd934295d0dc9c25d4f32c91612c8c
Kristof Matuszka <matuszkak@gmail.com>
2021-11-01 17:15:05 +0100

    2. feladat kész

--- counter.py
+++ counter.py
@@ -1,11 +1,8 @@
-from typing import Tuple
-
-
 class Counter:
     def __init__(self, value):
         self.value = value
         self.step = 1
-        print('\nCounter created!')
+        # print('\nCounter created!')
 
     def increment(self):
         self.value = self.value + self.step
@@ -32,7 +29,25 @@ class ScoreCounter(Counter):
         self.age = age
         self.winner = False
 
+    # increment and decrement functions customised with winner check step added
+    def increment(self):
+        self.value = self.value + self.step
+        self.winner = self.win_check()
 
+    def decrement(self):
+        self.value = self.value - self.step
+        self.winner = self.win_check()
+
+    # function checks is win value is reached
+    def win_check(self):
+        win_value = 12
+        if self.value >= win_value:
+            return True
+        else:
+            return False
+
+
+print(f'\n##### 2.1 #####')
 myCounter = Counter(10)
 myCounter.increment()
 myCounter.increment()
@@ -44,7 +59,9 @@ myCounter.set_value(100)
 myCounter.increment()
 myCounter.get_value()
 
+print(f'\n##### 2.2 #####')
 myScoreCounter = ScoreCounter(10, 'Zsolt', 34)
+
 myScoreCounter.increment()
 myScoreCounter.get_value()
 myScoreCounter.increment()
